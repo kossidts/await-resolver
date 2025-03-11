60bf805cf1188c86c65e8eb6da5f4f5a0feee63e
Kossi D. T. Saka <kdtsaka@gmail.com>
2025-03-11 08:48:19 +0100

    Add type declaration

---
+++ types/wrapper.d.mts
@@ -0,0 +1,3 @@
+export type ResolverResult<T_1> = import("./index.js").ResolverResult<T_1>;
+export type ResolverInput<T_1> = import("./index.js").ResolverInput<T_1>;
+export { default, default as resolver } from "./index.js";
--- wrapper.mjs
+++ wrapper.mjs
@@ -1,11 +1,15 @@
-/** @typedef {import('./index.js').ResolverResult<T>} ResolverResult<T> */
-/** @typedef {import('./index.js').ResolverInput<T>} ResolverInput<T> */
+/**
+ * @template T
+ * @typedef {import('./index.js').ResolverResult<T>} ResolverResult
+ */
 
 /**
  * @template T
- * @param {ResolverInput<T>} item - The value, promise or function to resolve
- * @param {number | null} [timeout] - Optional timeout in milliseconds
- * @param {...any} params - Optional parameters to pass to the function
- * @returns {Promise<ResolverResult<T>>} Returns a promise that resolves to [error, result]
+ * @typedef {import('./index.js').ResolverInput<T>} ResolverInput
+ */
+
+/**
+ * @template T
+ * @type {import('./index.js')<T>}
  */
 export { default, default as resolver } from "./index.js";
